a912829a33ddfc4e5aeb469d10a441f076577fe8
sanmoyano <smoyano.di@gmail.com>
2022-05-26 16:19:01 -0300

    first commit

---
+++ src/Theme/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme, theme } from "@chakra-ui/react";
+
+export default extendTheme({
+    config: {
+        initialColorModeName: "dark",
+    },
+
+    styles: {
+        global: {
+            "html, body, #root": {
+                height: "100%",
+            },
+        },
+    },
+});
